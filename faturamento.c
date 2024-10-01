b811d704ad363e1c57be561d1e92a7053ffb726d
Mateus Ávila <mateusmenavila@gmail.com>
2024-10-01 18:01:04 -0300

    Correcao arquivo json

--- faturamento.c
+++ faturamento.c
@@ -2,8 +2,13 @@
 
 int main(int argc, char const *argv[]) {
     double faturamento_diario[30] = {
-        1000.0, 0.0, 500.0, 0.0, 700.0, 1200.0, 300.0, 0.0, 850.0, 0.0, 400.0, 950.0, 0.0, 1200.0, 0.0, 1300.0, 0.0, 0.0, 800.0, 1500.0, 600.0, 0.0, 0.0, 750.0, 1400.0, 0.0, 1000.0, 0.0, 1700.0, 0.0};
- 
+        22174.1664, 24537.6698, 26139.6134, 0.0, 0.0, 26742.6612, 0.0, 
+        42889.2258, 46251.174, 11191.4722, 0.0, 0.0, 3847.4823, 373.7838, 
+        2659.7563, 48924.2448, 18419.2614, 0.0, 0.0, 35240.1826, 43829.1667, 
+        18235.6852, 4355.0662, 13327.1025, 0.0, 0.0, 25681.8318, 1718.1221, 
+        13220.495, 8414.61
+    };
+
     double menor_faturamento = -1, maior_faturamento = -1, soma_faturamento = 0;
     int dias_com_faturamento = 0, dias_acima_da_media = 0, num_dias = 30;
 
@@ -38,6 +43,7 @@ int main(int argc, char const *argv[]) {
 
     printf("Menor faturamento: %.2f\n", menor_faturamento);
     printf("Maior faturamento: %.2f\n", maior_faturamento);
+    printf("Média de faturamento: %.2f\n", media_faturamento);
     printf("Dias com faturamento acima da média: %d\n", dias_acima_da_media);
 
     return 0;
